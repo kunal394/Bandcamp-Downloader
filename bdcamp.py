5c39b4aba292fa51c5de5ed62d57aaab02477921
Kunal Singh <knlsingh394@gmail.com>
2016-07-10 17:51:13 +0530

    artist download functionality added.

--- bdcamp.py
+++ bdcamp.py
@@ -7,12 +7,28 @@
     https://popplers5.bandcamp.com/download/track?enc=mp3-128&fsig=66bf9341f2d63db3a4e203935b5a025c&id=2174833463&stream=1&ts=1465344506.0
      """
 
-import os, sys, requests, re, argparse
+import signal, time, os, sys, requests, re, argparse
 from bs4 import BeautifulSoup
 
 verbose = False
 automate = False
 noconfirm = False
+
+def exit_gracefully(signum, frame):
+    # restore the original signal handler as otherwise evil things will happen
+    # in raw_input when CTRL+C is pressed, and our signal handler is not re-entrant
+    signal.signal(signal.SIGINT, original_sigint)
+
+    try:
+        if raw_input("\nReally quit? (y/n)> ").lower().startswith('y'):
+            sys.exit(1)
+
+    except KeyboardInterrupt:
+        print("Ok ok, quitting")
+        sys.exit(1)
+
+    # restore the exit gracefully handler here    
+    signal.signal(signal.SIGINT, exit_gracefully)
 
 def vprint(arg):
     global verbose
@@ -21,6 +37,12 @@ def vprint(arg):
             print i
 
 def parse_url(url):
+
+    print """\n\nWarning!!! Either input a list of comma separated song nos
+           to download particular songs or \"all\" to download the
+           complete album. For example to download song nos 1, 2
+           and 4, input 1, 2, 4"""
+    print "Please adhere to the input format, otherwise the downloader won't behave as expected!!!\n\n"
 
     vprint(["Fetching data from the url provided..."])
 
@@ -34,18 +56,82 @@ def parse_url(url):
 
     dpath = []
     t = url.split('/')[3]
-    if t == 'track':
-        albumname = str(s.find("div", {"id" : "name-section"}).span.a.string.srtip())
-        dpath.append(albumname.title())
-    elif t == 'album' or t == '':
-        bandname=str(s.find("div", {"id" : "name-section"}).span.a.string.strip())
-        dpath.append(bandname.title())
-        albumname=str(s.find("div", {"id" : "name-section"}).h2.string.strip())
-        dpath.append(albumname.title())
-    vprint(["Parsing Complete"])
-    fetch_download_url(s, url, t, dpath)    
+    
+    vprint(["type: " + t])
 
-def fetch_download_url(s, url, urltype, dpath):
+    if t == 'track':
+        vprint(['in track'])
+        albumname = str(s.find("div", {"id" : "name-section"}).span.a.string.srtip().title())
+        dpath.append(albumname)
+        handle_track_album(s, dpath, {})
+    elif t == 'album' or t == '':
+        vprint(['in album'])
+        bandname = str(s.find("div", {"id" : "name-section"}).span.a.string.strip().title())
+        dpath.append(bandname)
+        albumname = str(s.find("div", {"id" : "name-section"}).h2.string.strip())
+        dpath.append(albumname)
+        handle_track_album(s, dpath, {})
+    elif t == 'music':
+        vprint(['in music'])
+        bandname = str(s.find("span", {"class" : "title"}).string.title())
+        dpath.append(bandname)
+        albumlist = [ str(i.string.strip().title()) for i in s.find_all("p", {"class" : "title"}) if i.string is not None ] 
+        handle_artist(albumlist, url, dpath)
+
+def handle_artist(albumlist, url, dpath):
+    album_dict = {}
+    for i in albumlist:
+        album_url = url[::-1].partition('/')[2][::-1] + "/album/" + i.lower().replace(' ', '-')
+
+        vprint(["\nFetching data from the album url provided..."])
+        r = requests.get(album_url)
+        vprint(["Data fetching complete", "Parsing data..."])
+        s = BeautifulSoup(r.text, 'html.parser')
+
+        vprint(["Fetching songs list for album: " + i + " from url: " + album_url])
+        print "\nSelect songs for the album: " + i
+        dl_dict = fetch_download_url(s, dpath)
+
+        if len(dl_dict) is not 0:
+            album_dict.update({i : dl_dict})
+
+    if len(album_dict) == 0:
+        print "No songs selected"
+    else:
+        print "List of the songs about to be downloaded:"
+        for i in album_dict:
+            print "\n\tAlbum Name: " + i
+            for j in album_dict[i]:
+                print "\t\tSong Name: " + album_dict[i][j][0]
+
+        for i in album_dict:
+            dpath.append(i)
+            print "\nDownloading songs selected in the album: " + i
+            handle_track_album(s.clear(), dpath, album_dict[i])
+            print "Album " + i + " downloaded"
+            dpath.remove(i)
+
+def handle_track_album(s, dpath, dl_dict):
+    if len(dl_dict) == 0:
+        dl_dict = fetch_download_url(s, dpath)    
+
+    if len(dl_dict) == 0:
+        print "No songs selected"
+    else:
+        print "Creating the directory structure... "
+        dirpath = "."
+        for i in dpath:
+            dirpath = dirpath + '/' + str(i)
+
+        if not os.path.exists(dirpath):
+            os.makedirs(dirpath)
+
+        for i in dl_dict:
+            print "Downloading " + str(i) + ': ' + str(dl_dict[i][0]) + "... "
+            download_song(str(dirpath) + '/' + str(dl_dict[i][0]) + '.mp3', dl_dict[i][1])
+            print "Downloading Complete"
+
+def fetch_download_url(s, dpath):
     global automate, noconfirm
 
     vprint(["Parsing songs list..."])
@@ -96,22 +182,8 @@ def fetch_download_url(s, url, urltype, dpath):
         vprint(["Fetching download url for the song: " + str(title)])
         dlurl = requests.get(furl, allow_redirects = False).headers['Location']
         dl_dict.update({i : [title, dlurl]})
-
-    if len(dl_list) == 0:
-        print "No songs selected"
-    else:
-        print "Creating the directory... "
-        dirpath = "./"
-        for i in dpath:
-            dirpath = dirpath + '/' + str(i)
-
-        if not os.path.exists(dirpath):
-            os.makedirs(dirpath)
-
-        for i in dl_list:
-            print "Downloading " + str(i) + ': ' + str(dl_dict[i][0]) + "... "
-            download_song(str(dirpath) + '/' + str(dl_dict[i][0]) + '.mp3', dl_dict[i][1])
-            print "Downloading Complete"
+    
+    return dl_dict
 
 def select_songs(songs_dict):
 
@@ -135,11 +207,6 @@ def display_songs(songs_dict):
     #print songs_dict
     for key in songs_dict:
         print str(key) + ": " + str(songs_dict[key][0])
-    print """Either input a list of comma separated song nos 
-    to download particular songs or \"all\" to download the 
-    complete album. For example to download song nos 1, 2 
-    and 4, input 1, 2, 4"""
-    print "Please adhere to the input format, otherwise the downloader won't work!!!"
     
     user_input = raw_input()
     if user_input.strip() == 'all':
@@ -147,6 +214,8 @@ def display_songs(songs_dict):
     else:
         dl_list = [int(s) for s in user_input.split(',') if s.strip().isdigit()]
     
+    dl_list = list(set(dl_list))
+
     print "You have selected following songs:"
     for i in dl_list:
         if i in songs_dict:
@@ -158,16 +227,32 @@ def display_songs(songs_dict):
 
     return dl_list
 
+def open_file(song_file):
+    while True:
+        try:
+            f = open(song_file, 'wb')
+        except IOError as e:
+            print e.errno
+            old_song_file = song_file
+            print "Invalid song name: " + song_file + "."
+            song_file = raw_input("Please provide a valid name along with the absolute path as present in the invalid name: ")
+            print "Changing file name from " + old_song_file + " to " + song_file 
+        else:
+            return f
 
 def download_song(song_file, url):
-    with open(song_file, 'wb')  as f:
-        data = requests.get(url, stream = True)
-        for chunk in data.iter_content(chunk_size=1024*1024):
-            if chunk:
-                print "len: " + str((len(chunk))/(1024*1024)) + "MB"
-                f.write(chunk)
-        f.close()
-        return data        
+
+    f = open_file(song_file)
+
+    print "Downloading " + song_file + "..."
+    data = requests.get(url, stream = True)
+    for chunk in data.iter_content(chunk_size=1024*1024):
+        if chunk:
+            print "len: " + str((len(chunk))/(1024*1024)) + "MB"
+            f.write(chunk)
+    f.close()
+    return data        
+
 
 
 #first download:https://oortcloudx.bandcamp.com/album/oort-cloud
@@ -176,7 +261,11 @@ if __name__ == "__main__":
     if (int(requests.__version__[0]) == 0):
         print ("Your version of requests needs updating\nTry: '(sudo) pip install -U requests'")
         sys.exit()
-    
+
+    # store the original SIGINT handler
+    original_sigint = signal.getsignal(signal.SIGINT)
+    signal.signal(signal.SIGINT, exit_gracefully)
+
     #argument parsing
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", help = "increase output verbosity", action = "store_true")
